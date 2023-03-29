102bc9eb1dcf8603bd629210863dc9558971627e
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2023-03-29 03:34:03 +0200

    last commit , all problmes fixed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 size = 40
 
-
+background_color = (81, 156, 83)
 class Apple:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
@@ -18,8 +18,8 @@ class Apple:
         pygame.display.flip()
 
     def move_apple(self):
-        self.x = size * random.randint(0, 24)
-        self.y = size * random.randint(0, 14)
+        self.x = size * random.randint(0,24)
+        self.y = size * random.randint(2,14)
 
 class Snake():
     def __init__(self, parent_screen, length):
@@ -37,7 +37,7 @@ class Snake():
 
 
     def draw(self):
-        self.parent_screen.fill((110, 110, 5))
+        self.parent_screen.fill((background_color))
         for i in range(self.length):
             self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
 
@@ -59,13 +59,17 @@ class Snake():
 
 class Game():
     def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.background_music()
         self.screen = pygame.display.set_mode((1000, 600))
-        self.screen.fill((110, 110, 5))
+        self.screen.fill((background_color))
         self.snake = Snake(self.screen, 2)
         self.snake.draw()
         self.apple = Apple(self.screen)
         self.apple.draw()
         self.hit = ''
+
 
     def is_collision(self, x1, x2, y1, y2):
         if x1 >= x2 and x1 < x2 + size:
@@ -73,6 +77,17 @@ class Game():
                 return True
         return  False
 
+    def background_music(self):
+        pygame.mixer.music.load("images/518917__bloodpixelhero__the-calm-theme.wav")
+        pygame.mixer.music.play()
+
+    def apple_snake_crash(self):
+        crash = False
+        for i in range(self.snake.length - 1, 0, -1):
+            if self.apple.x == self.snake.x[i] and self.apple.y == self.snake.y[i]:
+                crash = True
+                break
+        return crash
 
 
     def play(self):
@@ -82,12 +97,22 @@ class Game():
         pygame.display.flip()
         # eating apples
         if self.is_collision(self.apple.x, self.snake.x[0], self.apple.y, self.snake.y[0]):
+            sound = pygame.mixer.Sound("images/632232__audacitier__biting-apple-1.mp3")
+            pygame.mixer.Sound.play(sound)
             self.apple.move_apple()
             self.snake.increase_length()
+        while self.apple_snake_crash():
+            self.apple.move_apple()
         # collision of snake body
         for i in range(3, self.snake.length):
             if self.is_collision(self.snake.x[0], self.snake.x[i], self.snake.y[0], self.snake.y[i]):
+                sound = pygame.mixer.Sound("images/cymbal-crash-sound-effect.mp3")
+                pygame.mixer.Sound.play(sound)
                 raise "game over!"
+        if (1000 <= self.snake.x[0] or self.snake.x[0] < 0) or (600 <= self.snake.y[0] or self.snake.y[0] < 0):
+            sound = pygame.mixer.Sound("images/cymbal-crash-sound-effect.mp3")
+            pygame.mixer.Sound.play(sound)
+            raise  "game over!!"
 
     def display_score(self):
         font = pygame.font.SysFont('arial', 30)
@@ -95,7 +120,8 @@ class Game():
         self.screen.blit(score, (800, 10))
 
     def show_game_over(self):
-        self.screen.fill((110, 110, 5))
+        pygame.mixer.music.pause()
+        self.screen.fill((background_color))
         font = pygame.font.SysFont("arial", 30)
         line1 = font.render(f"GAME OVER!! : {self.snake.length-2}", True, (255, 255, 255))
         self.screen.blit(line1, (300, 200))
@@ -109,7 +135,6 @@ class Game():
 
 
     def run(self):
-        pygame.init()
         running = True
         pause = False
         while running:
@@ -118,6 +143,7 @@ class Game():
                     if event.key == K_ESCAPE:
                         running = False
                     if event.key == K_RETURN:
+                        self.background_music()
                         pause = False
                     if not pause:
                         if event.key == K_DOWN and self.snake.direction != 'up':
@@ -139,7 +165,7 @@ class Game():
                 self.show_game_over()
                 pause = True
                 self.reset()
-            time.sleep(.2)
+            time.sleep(.1)
 
 
 if __name__ == "__main__":
